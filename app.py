5582ddd812d87f4e532c56edf964f33c3643a152
Monisha Krothapalli <mkrothapalli123@gmail.com>
2025-03-25 23:51:33 -0400

    added very basic UI

---
+++ app.py
@@ -0,0 +1,17 @@
+import gradio as gr
+from PIL import Image
+from ml import process_images
+
+def display_images(image_paths):
+    images = [Image.open(image_path) for image_path in image_paths]
+    processed_images = process_images(images)
+    return processed_images
+
+image_demo = gr.Interface(
+    fn=display_images,
+    inputs=gr.Files(label="Upload Images", type="filepath"),
+    outputs=gr.Gallery(label="Uploaded Images"),
+    allow_flagging=False,
+)
+
+image_demo.launch(share=True)
---
+++ ml.py
@@ -0,0 +1,4 @@
+from PIL import Image
+
+def process_images(images): 
+    return None 
